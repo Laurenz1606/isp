9c6ec29df8607414e9e664b1994101340fb55fb2
root <root@vmd65580.contaboserver.net>
2021-07-06 10:31:31 +0200

    test

--- server/Routes/Provision.js
+++ server/Routes/Provision.js
@@ -3,4 +3,4 @@ const authenticateToken = require("./VerifyToken");
 const User = require("../Models/User");
 const Route = express.Router();
 
-Route.get("/getAll/:id")
+Route.get("/getAll/:id", authenticateToken, (req, res) => {});
