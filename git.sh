0e344f36be89278fe3e5df372fbcdcd0e65efb84
root <root@vmd65580.contaboserver.net>
2021-07-07 14:49:53 +0200

    test

--- git.sh
+++ git.sh
@@ -1,12 +1,16 @@
-while getopts m: flag
+while getopts m:t: flag
 do
     case "${flag}" in
         m) message=${OPTARG};;
+        t) task=${OPTARG};;
+
     esac
 done
-git add .
-git commit -m $message
-git push
-
-tree=$(git branch --show-current)
-echo "Commited files with message \"$message\" to branch $tree"
+if [ $task = "c" ]
+then
+  git add .
+  git commit -m $message
+  git push
+  tree=$(git branch --show-current)
+  echo "Commited files with message \"$message\" to branch $tree"
+fi
